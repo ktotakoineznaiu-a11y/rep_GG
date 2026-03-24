760c364682647ff68c8ea75518e3045710b70733
Marina <magubareva@sfedu.ru>
2026-03-24 14:49:52 +0300

    script added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function greet(name){
     return`Привет, ${name}!`;
     }
+    functionfarewell(name){
+    return`Пока, ${name}!`;
+    }
     console.log(greet('Мир'));
+    console.log(farewell('Друг'));
